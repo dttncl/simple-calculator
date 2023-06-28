afc70710326883917387d8aa2321c30d64ef9d0f
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-28 14:38:13 -0400

    modify calc display

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let keypads = document.querySelectorAll('button');
+let display = document.querySelector('#display');
 
 keypads.forEach(element => {
     element.classList.add('box');
 });
+
+console.log(display)
+display.textContent = '0';
