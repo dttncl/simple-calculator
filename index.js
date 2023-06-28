7c8ea81480a4ff84360cd72f8313ffd97be23197
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-28 16:38:53 -0400

    add reset functionality and chaining operation

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let keypads = document.querySelectorAll('button');
 let display = document.querySelector('#display');
 
-let numbers = document.querySelectorAll('.number');
-let operators = document.querySelectorAll('.operator');
-
 keypads.forEach(element => {
     element.classList.add('box');
 });
@@ -28,54 +25,66 @@ buildNumber();
 function buildNumber() {
     keypads.forEach (key => {
         key.addEventListener('click', () => { 
-            // check if value is a valid number
+
             if (Number(key.value) >= 0 && Number(key.value) <= 9) {
                 numBuilder += key.value;
+                display.textContent = numBuilder;
             } else if (key.value === '.' && dotCount === 0) {
                 numBuilder += key.value;
+                display.textContent = numBuilder;
                 dotCount = 1;
             } else if (OPERATOR.hasOwnProperty(key.value)) {
                 getOperator(key,numBuilder);
                 numBuilder = '';
             } else if (key.value === 'equal' && counter > 0) {
-                let res;
-                num2 = Number(numBuilder);
-                switch(op) {
-                    case '+':
-                        res = sum(num1,num2);
-                        break;
-                    case '-':
-                        res = subtract(num1,num2);
-                        break;
-                    case '*':
-                        res = multiply(num1,num2);
-                        break;
-                    case '/':
-                        res = divide(num1,num2);
-                        break;
-                }
-
-                display.textContent = res;
+                solve(num1);
+            } else if (key.value === 'ac') {
+                numBuilder = '';
+                num1 = 0;
+                num2 = 0;
+                op = '';
+                counter = 0;
+                dotCount = 0;
+                display.textContent = num1;
             }
+
         })
     })
-
-    
-
 }
 
 // function to save operator
 function getOperator(key,numBuilder) {
-    op = OPERATOR[key.value];
-
     if (counter === 0) {
+        op = OPERATOR[key.value];
         num1 = Number(numBuilder);
         counter = 1;
     } else {
-        num2 = Number(numBuilder);
+        num1 = solve(num1);
+        op = OPERATOR[key.value];
     }
 }
 
+// function to solve numbers
+function solve(num1) {
+    num2 = Number(numBuilder);
+    switch(op) {
+        case '+':
+            res = sum(num1,num2);
+            break;
+        case '-':
+            res = subtract(num1,num2);
+            break;
+        case '*':
+            res = multiply(num1,num2);
+            break;
+        case '/':
+            res = divide(num1,num2);
+            break;
+    }
+
+    display.textContent = res;
+    return res;
+}
 
 // functions for operators
 function sum(num1,num2) {
